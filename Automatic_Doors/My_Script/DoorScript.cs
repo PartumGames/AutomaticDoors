877a88a9249e06eb47db38a2e81eaaf8edd42d18
PartumGames <37095216+PartumGames@users.noreply.github.com>
2018-04-01 21:21:37 -0500

    Add files via upload

---
+++ Automatic_Doors/My_Script/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	public GameObject leftDoor;
+	public GameObject rightDoor;
+
+	Animator leftAnim;
+	Animator rightAnim;
+
+	void Start () {
+		leftAnim = leftDoor.GetComponent <Animator> ();
+		rightAnim = rightDoor.GetComponent <Animator> ();
+	}
+	
+	void OnTriggerEnter(Collider coll){
+		if (coll.tag == "Player") {
+			SlideDoors (true);
+		}
+	}
+
+	void OnTriggerExit(Collider coll){
+		if (coll.tag == "Player") {
+			SlideDoors (false);
+		}
+	}
+		
+	void SlideDoors(bool state){
+		leftAnim.SetBool ("slide", state);
+		rightAnim.SetBool ("slide", state);
+	}
+
+//end o class
+}
